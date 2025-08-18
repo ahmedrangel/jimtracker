a0220da9f532d5b0fd7d89a8a3de818ec755a2f7
Ahmed Rangel <ahmedrangel@outlook.com>
2025-08-18 12:07:49 -0500

    chore: migrate scroll behavior

--- app/app.vue
+++ app/app.vue
@@ -1,13 +1,9 @@
 <script setup lang="ts">
-const { options } = useRouter();
-options.scrollBehavior = (to) => {
-  if (!to.hash) return { left: 0, top: 0 };
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve({ el: to.hash, top: 55, left: 0, behavior: "smooth" });
-    }, 500);
-  });
-};
+const { $router } = useNuxtApp();
+$router.options.scrollBehavior = to => new Promise((resolve) => {
+  if (!to.hash) return resolve({ left: 0, top: 0 });
+  setTimeout(() => resolve({ el: to.hash, top: 55, left: 0, behavior: "smooth" }), 500);
+});
 </script>
 
 <template>
