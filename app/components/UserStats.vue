926f64f730e2501d0be4bc72f8d278c1773e641b
Ahmed Rangel <ahmedrangel@outlook.com>
2025-08-18 14:47:48 -0500

    chore: adjust responsiveness

--- app/components/UserStats.vue
+++ app/components/UserStats.vue
@@ -55,47 +55,47 @@ const socials = [
       <h2 class="text-2xl font-semibold">EST JimRising <span class="text-slate-400">#LANNN</span></h2>
     </div>
     <div class="mb-5 text-center bg-neutral-950/85 border border-slate-400/40 flex flex-col items-center w-[fit-content] place-self-center px-10 py-5 rounded">
-      <p class="text-xl font-semibold">ELO ACTUAL</p>
-      <p class="text-2xl font-bold flex items-center justify-center">
-        <img src="/images/lol/gold.png" class="w-16 h-16">
+      <p class="md:text-xl font-semibold">ELO ACTUAL</p>
+      <p class="text-lg md:text-2xl font-bold flex items-center justify-center">
+        <img src="/images/lol/gold.png" class="w-12 h-12 md:w-16 md:h-16">
         II · 12 LP
       </p>
     </div>
     <div class="space-y-4 mb-5">
       <div class="grid grid-cols-1 md:grid-cols-3 gap-2">
         <div class="bg-neutral-950/85 border border-emerald-300/40 rounded-lg py-4 px-10 text-center flex flex-col items-center justify-center">
-          <p class="text-lg text-emerald-200 font-semibold mb-1">ELO MÁXIMO</p>
-          <p class="text-xl text-emerald-200 font-bold flex items-center justify-center">
-            <img src="/images/lol/platinum.png" class="w-16 h-16">
+          <p class="md:text-lg text-emerald-200 font-semibold mb-1">ELO MÁXIMO</p>
+          <p class="md:text-xl text-emerald-200 font-bold flex items-center justify-center">
+            <img src="/images/lol/platinum.png" class="w-12 h-12 md:w-16 md:h-16">
             IV · {{ userStats.highestElo }} LP
           </p>
         </div>
         <div class="flex flex-col gap-2">
           <div class="bg-neutral-950/85 border border-purple-300/40 rounded-lg p-4 text-center flex flex-col items-center justify-center">
-            <p class="text-lg text-purple-200 font-semibold mb-1">WINRATE</p>
-            <p class="text-xl text-purple-200 font-bold flex items-center justify-center">
+            <p class="md:text-lg text-purple-200 font-semibold mb-1">WINRATE</p>
+            <p class="md:text-xl text-purple-200 font-bold flex items-center justify-center">
               64.2%
             </p>
           </div>
           <div class="flex items-center justify-center gap-2">
             <div class="bg-neutral-950/85 border border-emerald-300/40 rounded-lg p-4 text-center flex flex-col items-center justify-center w-full">
-              <p class="text-lg text-emerald-200 font-semibold mb-1">VICTORIAS</p>
-              <p class="text-xl text-emerald-200 font-bold flex items-center justify-center">
+              <p class="md:text-lg text-emerald-200 font-semibold mb-1">VICTORIAS</p>
+              <p class="md:text-xl text-emerald-200 font-bold flex items-center justify-center">
                 100
               </p>
             </div>
             <div class="bg-neutral-950/85 border border-rose-300/40 rounded-lg p-4 text-center flex flex-col items-center justify-center w-full">
-              <p class="text-lg text-rose-200 font-semibold mb-1">DERROTAS</p>
-              <p class="text-xl text-rose-200 font-bold flex items-center justify-center">
+              <p class="md:text-lg text-rose-200 font-semibold mb-1">DERROTAS</p>
+              <p class="md:text-xl text-rose-200 font-bold flex items-center justify-center">
                 100
               </p>
             </div>
           </div>
         </div>
         <div class="bg-neutral-950/85 border border-rose-300/40 rounded-lg py-4 px-10 text-center flex flex-col items-center justify-center">
-          <p class="text-lg text-rose-200 font-semibold mb-1">ELO MÍNIMO</p>
-          <p class="text-xl text-rose-200 font-bold flex items-center justify-center">
-            <img src="/images/lol/silver.png" class="w-16 h-16">
+          <p class="md:text-lg text-rose-200 font-semibold mb-1">ELO MÍNIMO</p>
+          <p class="md:text-xl text-rose-200 font-bold flex items-center justify-center">
+            <img src="/images/lol/silver.png" class="w-12 h-12 md:w-16 md:h-16">
             IV · {{ userStats.lowestElo }} LP
           </p>
         </div>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -4,7 +4,7 @@
 <template>
   <main>
     <div class="flex items-center justify-center">
-      <div class="mb-5 p-4 bg-red-800 rounded aspect-square flex items-center justify-center w-36 h-36">
+      <div class="mb-5 p-4 bg-red-800 rounded aspect-square flex items-center justify-center w-30 h-30 md:w-36 md:h-36">
         <img src="/images/jimrsng.png" class="max-w-full max-h-full object-contain">
       </div>
     </div>
