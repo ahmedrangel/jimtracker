eab688413e6b6355a53ab320a0ca5e22ba9e3ec9
Ahmed Rangel <ahmedrangel@outlook.com>
2025-08-18 11:27:34 -0500

    feat: add bg image

--- app/components/UserStats.vue
+++ app/components/UserStats.vue
@@ -37,66 +37,66 @@ const getRankColor = (rank: string) => {
     <div class="text-center mb-5">
       <div class="flex justify-center gap-4 mb-5">
         <NuxtLink external target="_blank" to="https://www.twitch.tv/jimrsng">
-          <Icon name="simple-icons:twitch" class="text-purple-400" size="20" />
+          <Icon name="simple-icons:twitch" class="text-purple-400" size="22" />
         </NuxtLink>
         <NuxtLink external target="_blank" to="https://x.com/JimRisingSC">
-          <Icon name="simple-icons:x" class="text-white-500" size="20" />
+          <Icon name="simple-icons:x" class="text-white-500" size="22" />
         </NuxtLink>
         <NuxtLink external target="_blank" to="https://www.instagram.com/Jimrising12">
-          <Icon name="simple-icons:instagram" class="text-pink-400" size="20" />
+          <Icon name="simple-icons:instagram" class="text-pink-400" size="22" />
         </NuxtLink>
         <NuxtLink external target="_blank" to="https://www.tiktok.com/@jimrising">
-          <Icon name="simple-icons:tiktok" class="text-white-500" size="20" />
+          <Icon name="simple-icons:tiktok" class="text-white-500" size="22" />
         </NuxtLink>
         <NuxtLink external target="_blank" to="https://www.facebook.com/JimRisingSC2">
-          <Icon name="simple-icons:facebook" class="text-blue-400" size="20" />
+          <Icon name="simple-icons:facebook" class="text-blue-400" size="22" />
         </NuxtLink>
         <NuxtLink external target="_blank" to="https://www.youtube.com/channel/UCyLusY-ST8KxpXe1A67r37A">
-          <Icon name="simple-icons:youtube" class="text-rose-400" size="20" />
+          <Icon name="simple-icons:youtube" class="text-rose-400" size="22" />
         </NuxtLink>
       </div>
       <h2 class="text-2xl font-semibold">EST JimRising <span class="text-slate-400">#LANNN</span></h2>
     </div>
-    <div class="mb-5 text-center bg-slate-950/70 border border-yellow-700/50 flex flex-col items-center w-[fit-content] place-self-center px-10 py-4 rounded">
-      <p class="text-xl font-semibold">Elo Actual</p>
-      <p class="text-xl font-bold flex items-center justify-center">
+    <div class="mb-5 text-center bg-black/80 border border-slate-500/50 flex flex-col items-center w-[fit-content] place-self-center px-10 py-5 rounded">
+      <p class="text-2xl font-semibold">ELO ACTUAL</p>
+      <p class="text-2xl font-bold flex items-center justify-center">
         <img src="/images/lol/gold.png" class="w-16 h-16">
         II · 12 LP
       </p>
     </div>
     <div class="space-y-4 mb-5">
       <div class="grid grid-cols-1 md:grid-cols-3 gap-2">
-        <div class="bg-emerald-50 rounded-lg py-4 px-10 text-center flex flex-col items-center justify-center">
-          <p class="text-sm text-emerald-900 font-semibold mb-1">Elo Máximo</p>
+        <div class="bg-emerald-100 rounded-lg py-4 px-10 text-center flex flex-col items-center justify-center">
+          <p class="text-sm text-emerald-900 font-semibold mb-1">ELO MÁXIMO</p>
           <p class="text-xl font-bold text-emerald-900 flex items-center justify-center">
             <img src="/images/lol/platinum.png" class="w-16 h-16">
             IV · {{ userStats.highestElo }} LP
           </p>
         </div>
         <div class="flex flex-col gap-2">
-          <div class="bg-purple-50 rounded-lg p-4 text-center flex flex-col items-center justify-center">
-            <p class="text-sm text-purple-900 font-semibold mb-1">Winrate</p>
+          <div class="bg-purple-100 rounded-lg p-4 text-center flex flex-col items-center justify-center">
+            <p class="text-sm text-purple-900 font-semibold mb-1">WINRATE</p>
             <p class="text-xl font-bold text-purple-900 flex items-center justify-center">
               64.2%
             </p>
           </div>
           <div class="flex items-center justify-center gap-2">
-            <div class="bg-emerald-50 rounded-lg p-4 text-center flex flex-col items-center justify-center w-full">
-              <p class="text-sm text-emerald-900 font-semibold mb-1">Victorias</p>
+            <div class="bg-emerald-100 rounded-lg p-4 text-center flex flex-col items-center justify-center w-full">
+              <p class="text-sm text-emerald-900 font-semibold mb-1">VICTORIAS</p>
               <p class="text-xl font-bold text-emerald-900 flex items-center justify-center">
                 100
               </p>
             </div>
-            <div class="bg-rose-50 rounded-lg p-4 text-center flex flex-col items-center justify-center w-full">
-              <p class="text-sm text-rose-900 font-semibold mb-1">Derrotas</p>
+            <div class="bg-rose-100 rounded-lg p-4 text-center flex flex-col items-center justify-center w-full">
+              <p class="text-sm text-rose-900 font-semibold mb-1">DERROTAS</p>
               <p class="text-xl font-bold text-rose-900 flex items-center justify-center">
                 100
               </p>
             </div>
           </div>
         </div>
-        <div class="bg-rose-50 rounded-lg py-4 px-10 text-center flex flex-col items-center justify-center">
-          <p class="text-sm text-rose-900 font-semibold mb-1">Elo Mínimo</p>
+        <div class="bg-rose-100 rounded-lg py-4 px-10 text-center flex flex-col items-center justify-center">
+          <p class="text-sm text-rose-900 font-semibold mb-1">ELO MÍNIMO</p>
           <p class="text-xl font-bold text-rose-900 flex items-center justify-center">
             <img src="/images/lol/silver.png" class="w-16 h-16">
             IV · {{ userStats.lowestElo }} LP
