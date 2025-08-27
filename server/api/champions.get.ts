0babf358721f13b4e882b4ef96556b71a02cbe5d
Ahmed Rangel <ahmedrangel@outlook.com>
2025-08-26 22:41:32 -0500

    chore: update deps

--- server/api/champions.get.ts
+++ server/api/champions.get.ts
@@ -1,15 +1,11 @@
-import { Dto, LolApi } from "twisted";
+import { LolApi } from "twisted";
 
 export default defineCachedEventHandler(async () => {
   const lol = new LolApi();
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
-  const ChampionsDataDragon = new Dto.ChampionsDataDragon();
-  const version = await lol.DataDragon.getVersions();
-  const { data } = await $fetch<typeof ChampionsDataDragon>(`https://ddragon.leagueoflegends.com/cdn/${version[0]}/data/es_MX/champion.json`);
+  const { data } = await lol.DataDragon.getChampionList("es_MX");
   const champions = Object.values(data).map(champion => ({
     id: champion.key,
     name: champion.name
-
   }));
   return champions;
 }, {
