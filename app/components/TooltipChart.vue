681d69e10aea88d9a812cedf994765886c7c05a6
Ahmed Rangel <ahmedrangel@outlook.com>
2025-08-31 15:15:05 -0500

    chore: adjust chart tooltip styling

--- app/components/TooltipChart.vue
+++ app/components/TooltipChart.vue
@@ -17,24 +17,26 @@ const defaultStyle = {
 </script>
 
 <template>
-  <div :style="style ? style : defaultStyle" class="text-center text-xs md:text-base absolute bg-slate-900/95 border border-blue-500 rounded-lg w-max overflow-hidden pt-1.5 px-0 pointer-events-none z-100 transition-all duration-300 ease-in-out">
+  <div :style="style ? style : defaultStyle" class="text-center text-xs md:text-base absolute border border-blue-500/80 rounded-lg w-max overflow-hidden px-0 pointer-events-none z-100 transition-all duration-300 ease-in-out">
     <template v-if="content">
       <template v-if="type === 'daily' && Array.isArray(content.data)">
-        <div class="text-base font-bold px-3">{{ content.label }}</div>
-        <div class="px-3 flex items-center justify-center font-semibold">
-          <img :src="`/images/lol/${content.dataTier?.toLowerCase()}.png`" class="w-10 h-10">
-          <span>{{ content.rankDisplay }}</span>
-        </div>
-        <div class=" px-3 font-semibold mb-1.5">
-          <span class="flex items-center gap-1 justify-center">
-            <span>
-              {{ content.changeText }} LP
+        <div class="bg-slate-900/95 py-1.5">
+          <div class="text-base font-bold px-3">{{ content.label }}</div>
+          <div class="px-3 flex items-center justify-center font-semibold">
+            <img :src="`/images/lol/${content.dataTier?.toLowerCase()}.png`" class="w-10 h-10">
+            <span>{{ content.rankDisplay }}</span>
+          </div>
+          <div class=" px-3 font-semibold">
+            <span class="flex items-center gap-1 justify-center">
+              <span>
+                {{ content.changeText }} LP
+              </span>
+              <Icon :name="content.changeIcon" :class="content.changeColor" size="26" />
             </span>
-            <Icon :name="content.changeIcon" :class="content.changeColor" size="26" />
-          </span>
+          </div>
         </div>
-        <div class="text-sm px-3 font-semibold bg-neutral-950 py-1">Partidas: {{ content.data.length }}</div>
-        <div v-for="data in content.data" :key="data.champion_id + '-' + data.date" :class="data.is_remake ? 'bg-neutral-800' : data.result ? 'bg-green-900' : 'bg-red-900'">
+        <div class="text-sm px-3 font-semibold bg-neutral-950/95 py-1">Partidas: {{ content.data.length }}</div>
+        <div v-for="data in content.data" :key="data.champion_id + '-' + data.date" :class="data.is_remake ? 'bg-neutral-800/95' : data.result ? 'bg-green-900/95' : 'bg-red-900/95'">
           <span class="flex items-center px-3 py-1.5 gap-1.5">
             <img :src="getChampionIcon(data.champion_id)" :alt="data.champion" class="w-6 h-6 rounded border border-slate-700" style="object-fit: cover;">
             <span>{{ data.kills }}/{{ data.deaths }}/{{ data.assists }} · {{ format(data.date, "h:mm aa") }}</span>
@@ -42,20 +44,22 @@ const defaultStyle = {
         </div>
       </template>
       <template v-if="type === 'match' && !Array.isArray(content.data)">
-        <div class="text-base font-bold px-3">{{ format(content.data.date, "dd MMM yyyy", { locale: es }) }} · {{ format(content.data.date, "h:mm aa") }}</div>
-        <div class="px-3 flex items-center justify-center font-semibold">
-          <img :src="`/images/lol/${content.data.tier?.toLowerCase()}.png`" class="w-10 h-10">
-          <span>{{ content.rankDisplay }}</span>
-        </div>
-        <div class=" px-3 font-semibold mb-1.5">
-          <span class="flex items-center gap-1 justify-center">
-            <span>
-              {{ content.changeText }} LP
+        <div class="bg-slate-900/95 py-1.5">
+          <div class="text-base font-bold px-3">{{ format(content.data.date, "dd MMM yyyy", { locale: es }) }} · {{ format(content.data.date, "h:mm aa") }}</div>
+          <div class="px-3 flex items-center justify-center font-semibold">
+            <img :src="`/images/lol/${content.data.tier?.toLowerCase()}.png`" class="w-10 h-10">
+            <span>{{ content.rankDisplay }}</span>
+          </div>
+          <div class=" px-3 font-semibold">
+            <span class="flex items-center gap-1 justify-center">
+              <span>
+                {{ content.changeText }} LP
+              </span>
+              <Icon :name="content.changeIcon" :class="content.changeColor" size="26" />
             </span>
-            <Icon :name="content.changeIcon" :class="content.changeColor" size="26" />
-          </span>
+          </div>
         </div>
-        <div class="flex items-center justify-center px-3 py-1.5 gap-1.5" :class="content.data.is_remake ? 'bg-neutral-800' : content.data.result ? 'bg-green-900' : 'bg-red-900'">
+        <div class="flex items-center justify-center px-3 py-1.5 gap-1.5" :class="content.data.is_remake ? 'bg-neutral-800/95' : content.data.result ? 'bg-green-900/95' : 'bg-red-900/95'">
           <img :src="getChampionIcon(content.data.champion_id)" :alt="content.data.champion" class="w-12 h-12 rounded border border-slate-700" style="object-fit: cover;">
           <span>{{ content.data.kills }}/{{ content.data.deaths }}/{{ content.data.assists }}</span>
         </div>
