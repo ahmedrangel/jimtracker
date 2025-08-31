e149f5ad80b924416995bc7e486e505232444962
Ahmed Rangel <ahmedrangel@outlook.com>
2025-08-30 19:04:33 -0500

    Update UserStats.vue

--- app/components/UserStats.vue
+++ app/components/UserStats.vue
@@ -39,7 +39,7 @@ defineProps<{
               <span class="text-sm text-slate-400 font-semibold">
                 <span class="text-emerald-300">{{ champ.wins }}</span>V · <span class="text-rose-300">{{ champ.losses }}</span>D (<span class="text-slate-100">{{ champ.count }}</span>)
               </span>
-              <span class="text-sm text-slate-100 font-semibold">{{ ((champ.wins / champ.count) * 100).toFixed(2) }}% WR</span>
+              <span class="text-sm text-slate-100 font-semibold">{{ ((champ.wins / champ.count) * 100).toFixed(1) }}% WR</span>
               <span class="text-xs text-slate-400 font-semibold">
                 <span class="text-emerald-300">{{ champ.kills.toFixed(0) }}</span> / <span class="text-rose-300">{{ champ.deaths.toFixed(0) }}</span> / <span class="text-yellow-200">{{ champ.assists.toFixed(0) }}</span> (<span class="text-slate-100">{{ champ.deaths > 0 ? ((Number(champ.kills.toFixed(0)) + Number(champ.assists.toFixed(0))) / (Number(champ.deaths.toFixed(0)))).toFixed(2) : "Perfect" }}</span> KDA)
               </span>
