2a622a7b3627d6172bc543eb12c719fee6124449
Yizack Rangel <yizackr@gmail.com>
2025-09-11 22:15:15 +0200

    chore: remove user agent

--- server/api/gallery/index.post.ts
+++ server/api/gallery/index.post.ts
@@ -18,12 +18,7 @@ export default defineEventHandler(async (event) => {
   let blob: File | Blob;
 
   if (body.url) {
-    blob = await $fetch(body.url, {
-      responseType: "blob",
-      headers: {
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/139.0.0.0 Safari/537.36"
-      }
-    });
+    blob = await $fetch(body.url, { responseType: "blob" });
   }
   else {
     const files = await readFormData(event);
