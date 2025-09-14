cc6d73a33b6b008e9229e910985506cad8c99ddf
Ahmed Rangel <ahmedrangel@outlook.com>
2025-09-14 14:35:37 -0500

    chore: adjust update.post api

--- server/api/update.post.ts
+++ server/api/update.post.ts
@@ -1,6 +1,7 @@
 export default defineEventHandler(async (): Promise<InfoResponse> => {
   const now = Date.now();
   const checkInfo = await useStorage("cache").getItem<UserInfo>("info");
+  const dbInfo = await getDBInfo();
   if (checkInfo && (now - checkInfo.updatedAt < 2 * 60 * 1000)) {
     const dbInfo = await getDBInfo();
     return {
@@ -8,10 +9,7 @@ export default defineEventHandler(async (): Promise<InfoResponse> => {
       ...dbInfo
     };
   }
-  const [riotPolling, dbInfo] = await Promise.all([
-    runTask<UserInfo>("riotPolling"),
-    getDBInfo()
-  ]);
+  const riotPolling = await runTask<UserInfo>("riotPolling");
   if (riotPolling?.result) await useStorage("cache").setItem("info", { ...riotPolling.result, updatedAt: now });
   const info = { ...riotPolling.result!, updatedAt: now };
   return { user: info, ...dbInfo };
