6e2eb2dfae07e74471c4c119055ef1cd2eb920db
Ahmed Rangel <ahmedrangel@outlook.com>
2025-09-17 04:21:16 -0500

    fix(update.post): get db info after running riot polling

--- server/api/update.post.ts
+++ server/api/update.post.ts
@@ -1,7 +1,6 @@
 export default defineEventHandler(async (event): Promise<InfoResponse> => {
   const now = Date.now();
   const checkInfo = await useStorage("cache").getItem<UserInfo>("info");
-  const dbInfo = await getDBInfo();
   if (checkInfo && (now - checkInfo.updatedAt < 2 * 60 * 1000)) {
     const dbInfo = await getDBInfo();
     return {
@@ -12,6 +11,7 @@ export default defineEventHandler(async (event): Promise<InfoResponse> => {
   const config = useRuntimeConfig(event);
   const riotPolling = await runTask<UserInfo>("riotPolling");
   const rankedData = await fetchRankedData(config);
+  const dbInfo = await getDBInfo();
   if (riotPolling?.result) await useStorage("cache").setItem("info", { ...riotPolling.result, ...rankedData, updatedAt: now });
   const info = { ...riotPolling.result!, ...rankedData, updatedAt: now };
   return { user: info, ...dbInfo };
