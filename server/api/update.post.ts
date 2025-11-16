872d3241961a5ac486f97592332a286bc8de83d1
Ahmed Rangel <ahmedrangel@outlook.com>
2025-11-16 17:53:41 -0500

    fix: include liveInfo in user info response update post

--- server/api/update.post.ts
+++ server/api/update.post.ts
@@ -5,7 +5,10 @@ export default defineEventHandler(async (event): Promise<InfoResponse> => {
   const puuid = soloboom ? constants.soloboomPuuids[2025] : constants.riotPuuid;
   const key = soloboom ? "info-soloboom" : "info";
   const pollingKey = soloboom ? "soloboomPolling" : "riotPolling";
-  const checkInfo = await useStorage("cache").getItem<UserInfo>(key);
+  const [checkInfo, liveInfo] = await Promise.all([
+    useStorage("cache").getItem<UserInfo>(key),
+    useStorage("cache").getItem<LiveInfo>("live-info")
+  ]);
   if (checkInfo && (now - checkInfo.updatedAt < 2 * 60 * 1000)) {
     const dbInfo = await getDBInfo(puuid);
     return {
@@ -21,6 +24,6 @@ export default defineEventHandler(async (event): Promise<InfoResponse> => {
   ]);
   const dbInfo = await getDBInfo(puuid);
   if (pollingData?.result) await useStorage("cache").setItem(key, { ...pollingData.result, ...rankedData, ...userData, updatedAt: now });
-  const info = { ...pollingData.result!, ...rankedData, ...userData, updatedAt: now };
+  const info = { ...pollingData.result!, ...rankedData, ...userData, ...liveInfo, updatedAt: now };
   return { user: info, ...dbInfo };
 });
