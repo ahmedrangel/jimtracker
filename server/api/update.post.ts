ac3720a642e217d3347deeb84e9a2b18efd6f83a
Ahmed Rangel <ahmedrangel@outlook.com>
2025-11-16 18:25:25 -0500

    Update update.post.ts

--- server/api/update.post.ts
+++ server/api/update.post.ts
@@ -18,12 +18,11 @@ export default defineEventHandler(async (event): Promise<InfoResponse> => {
   }
   const config = useRuntimeConfig(event);
   const pollingData = await runTask<UserInfo>(pollingKey);
-  const [rankedData, liveData] = await Promise.all([
-    fetchRankedData(config, puuid),
-    fetchLiveData(config, puuid)
+  const [userData] = await Promise.all([
+    fetchUserData(config, puuid)
   ]);
   const dbInfo = await getDBInfo(puuid);
-  if (pollingData?.result) await useStorage("cache").setItem(key, { ...pollingData.result, ...rankedData, ...liveData, updatedAt: now });
-  const info = { ...pollingData.result!, ...rankedData, ...liveData, ...liveInfo, updatedAt: now };
+  if (pollingData?.result) await useStorage("cache").setItem(key, { ...pollingData.result, ...userData, updatedAt: now });
+  const info = { ...pollingData.result!, ...userData, ...liveInfo, updatedAt: now };
   return { user: info, ...dbInfo };
 });
