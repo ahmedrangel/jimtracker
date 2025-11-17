43d69db7bb9b9b2e02ef37af05c14e0176bd819a
Ahmed Rangel <ahmedrangel@outlook.com>
2025-11-16 20:10:12 -0500

    Update update.post.ts

--- server/api/update.post.ts
+++ server/api/update.post.ts
@@ -5,10 +5,8 @@ export default defineEventHandler(async (event): Promise<InfoResponse> => {
   const puuid = soloboom ? constants.soloboomPuuids[2025] : constants.riotPuuid;
   const key = soloboom ? "info-soloboom" : "info";
   const pollingKey = soloboom ? "soloboomPolling" : "riotPolling";
-  const [checkInfo, liveGame, LiveStreamInfo] = await Promise.all([
-    useStorage("cache").getItem<UserInfo>(key),
-    useStorage("cache").getItem<LiveGame>(`live:${key}`),
-    useStorage("cache").getItem<LiveStreamInfo>("live-info")
+  const [checkInfo] = await Promise.all([
+    useStorage("cache").getItem<UserInfo>(key)
   ]);
   if (checkInfo && (now - checkInfo.updatedAt < 2 * 60 * 1000)) {
     const dbInfo = await getDBInfo(puuid);
@@ -24,6 +22,10 @@ export default defineEventHandler(async (event): Promise<InfoResponse> => {
   ]);
   const dbInfo = await getDBInfo(puuid);
   if (pollingData?.result) await useStorage("cache").setItem(key, { ...pollingData.result, ...userData, updatedAt: now });
-  const info = { ...pollingData.result!, ...userData, ...liveGame, ...LiveStreamInfo, updatedAt: now };
+  const [liveGame, liveStreamInfo] = await Promise.all([
+    useStorage("cache").getItem<LiveGame>(`live:${key}`),
+    useStorage("cache").getItem<LiveStreamInfo>("live-info")
+  ]);
+  const info = { ...pollingData.result!, ...userData, ...liveStreamInfo, ...liveGame, updatedAt: now };
   return { user: info, ...dbInfo };
 });
