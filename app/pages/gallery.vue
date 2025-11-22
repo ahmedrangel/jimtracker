c2a31574f34f30852d35eb488b88441444db5853
Ahmed Rangel <ahmedrangel@outlook.com>
2025-11-22 00:43:35 -0500

    chore: adjust

--- app/pages/gallery.vue
+++ app/pages/gallery.vue
@@ -22,7 +22,7 @@ const openImageId = ref<string | null>(null);
       </div>
       <template #content>
         <div class="relative">
-          <img v-if="openImageId" :src="`${SITE.cdn}/gallery/${openImageId}`" class="rounded-lg min-w-[50vh] max-h-[90vh]">
+          <img v-if="openImageId" :src="`${SITE.cdn}/gallery/${openImageId}`" class="rounded-lg max-w-[90vw] max-h-[80vh] md:min-h-[50vh]">
           <Icon name="tabler:x" class="absolute top-2 end-2 w-8 h-8 cursor-pointer text-neutral-200 hover:text-neutral-300 bg-neutral-900/70 hover:bg-neutral-900 rounded p-1.5" @click="openImageId = null; showModal = false;" />
         </div>
       </template>
