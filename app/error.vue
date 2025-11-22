a9c9d1bca39af3c07fb04c2ab4e16f9a06b29a1e
Ahmed Rangel <ahmedrangel@outlook.com>
2025-11-21 23:34:27 -0500

    chore: adjust class

--- app/error.vue
+++ app/error.vue
@@ -7,7 +7,7 @@ defineProps<{
 </script>
 
 <template>
-  <div class="w-[100vw] h-[100vh] flex justify-center items-center bg-neutral-400 dark:bg-neutral-950 text-lg">
+  <div class="w-screen h-screen flex justify-center items-center bg-neutral-400 dark:bg-neutral-950 text-lg">
     <div class="p-5 shadow bg-neutral-100 dark:bg-neutral-800 rounded">
       <h1>Error <span class="text-primary">{{ error.statusCode }}</span></h1>
       <h5>{{ "error_occurred" }}: {{ error.message || error.statusMessage }}</h5>
