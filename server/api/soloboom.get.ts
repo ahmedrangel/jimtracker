92058b84515295649eb8b2eff7d7f8d066c670cc
Ahmed Rangel <ahmedrangel@outlook.com>
2025-11-24 02:42:00 -0500

    refactor: use cached event handler on soloboom api endpoint

--- server/api/soloboom.get.ts
+++ server/api/soloboom.get.ts
@@ -1,25 +1,25 @@
 import { load } from "cheerio";
 
-export default defineEventHandler(async () => {
+export default defineCachedEventHandler(async () => {
   const storage = useStorage("cache");
-  const soloBoomRankData = await storage.getItem<{ rank: string, updatedAt: number }>("soloboom-rank");
   const info = await storage.getItem<UserLeague>("info-soloboom");
-  if (!info) return;
-  const now = Date.now();
-  if (soloBoomRankData && (now - soloBoomRankData.updatedAt < 5 * 60 * 1000)) { // 5 minutes
-    return {
-      rank: soloBoomRankData.rank
-    };
-  }
+  if (!info) {
+    throw createError({ statusCode: 404, statusMessage: "SoloBoom info not found", fatal: true });
+  };
   const soloboomScrape = await $fetch<string>("https://soloboom.net/leaderboard", { responseType: "text", timeout: 5000 }).catch(() => null);
-  if (soloboomScrape) {
-    const html = load(soloboomScrape);
-    const row = html(`td:contains(${info.gameName}-#${info.tagLine})`).parent();
-    const soloboomRank = row.find("td").eq(0).text().trim();
-    const updatedAt = Date.now();
-    await storage.setItem<{ rank: string, updatedAt: number }>("soloboom-rank", { rank: soloboomRank || "", updatedAt });
-    return {
-      rank: soloboomRank
-    };
+  if (!soloboomScrape) {
+    throw createError({ statusCode: 503, statusMessage: "SoloBoom leaderboard is currently unavailable", fatal: true });
   }
+  const html = load(soloboomScrape);
+  const row = html(`td:contains(${info.gameName}-#${info.tagLine})`).parent();
+  const soloboomRank = row.find("td").eq(0).text().trim();
+  return {
+    rank: soloboomRank
+  };
+}, {
+  maxAge: 5 * 60 * 1000, // 5 minutes
+  swr: false,
+  getKey: () => "rank",
+  group: "api",
+  name: "soloboom"
 });
