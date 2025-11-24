37459a610570d30ced178d8cf44d8e27db864a4d
Ahmed Rangel <ahmedrangel@outlook.com>
2025-11-24 14:04:24 -0500

    chore: update gallery modal close icon style

--- app/pages/gallery.vue
+++ app/pages/gallery.vue
@@ -23,7 +23,7 @@ const openImageId = ref<string | null>(null);
       <template #content>
         <div v-if="openImageId" class="relative">
           <img :src="`${SITE.cdn}/gallery/${openImageId}`" class="rounded-lg max-h-[90vh] max-w-[90vw] sm:min-w-[600px] md:min-w-[700px]">
-          <Icon name="tabler:x" class="absolute top-2 end-2 w-8 h-8 text-neutral-200 hover:text-neutral-300 bg-neutral-900/70 hover:bg-neutral-900 rounded p-1.5" @click="openImageId = null; showModal = false;" />
+          <Icon name="tabler:x" class="absolute top-2 end-2 w-8 h-8 text-neutral-200 hover:text-neutral-300 bg-rose-700/70 hover:bg-rose-700 border border-rose-800 rounded p-1.5 shadow" @click="openImageId = null; showModal = false;" />
         </div>
       </template>
     </UModal>
