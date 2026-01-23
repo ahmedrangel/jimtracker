973ba7b696c63ad60e868565b1c3ae5ce61a109f
Ahmed Rangel <ahmedrangel@outlook.com>
2026-01-22 21:35:55 -0500

    chore: remove unecessary external

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -42,9 +42,9 @@ const solobooms: DropdownMenuItem[] = [
         <slot />
         <footer class="text-center text-sm text-slate-300">
           <span>
-            <NuxtLink href="https://github.com/ahmedrangel/jimtracker" target="_blank" external class="text-emerald-300 font-semibold hover:underline">Creado</NuxtLink>
+            <NuxtLink href="https://github.com/ahmedrangel/jimtracker" target="_blank" class="text-emerald-300 font-semibold hover:underline">Creado</NuxtLink>
             con ❤️ por
-            <NuxtLink href="https://github.com/ahmedrangel" target="_blank" external class="text-emerald-300 font-semibold hover:underline">Ahmed Rangel</NuxtLink>
+            <NuxtLink href="https://github.com/ahmedrangel" target="_blank" class="text-emerald-300 font-semibold hover:underline">Ahmed Rangel</NuxtLink>
           </span>
         </footer>
       </div>
