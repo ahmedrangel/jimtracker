8d5a37967c2b9e69b2ac30d0b43884bd8e5624fa
Ahmed Rangel <ahmedrangel@outlook.com>
2026-02-15 13:31:18 -0500

    chore: move nuxthub exports to new utils file

--- server/utils/db.ts
+++ server/utils/db.ts
@@ -2,6 +2,3 @@ import * as schema from "../db/schema";
 
 export { sql, eq, and, or, count, desc, asc, exists, notExists, gte, isNotNull } from "drizzle-orm";
 export const tables = schema;
-
-export { db } from "@nuxthub/db";
-export { blob } from "@nuxthub/blob";
