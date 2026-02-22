48b05a8f1a0ae1d125cb4f8365e9392cd4d67e83
Ahmed Rangel <ahmedrangel@outlook.com>
2026-02-21 20:18:27 -0500

    chore: lint

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -122,48 +122,48 @@ const timeAgo = useTimeAgoIntl(buildInfo.time, {
         <slot />
       </div>
     </div>
-  <UFooter class="border-t border-default">
-    <template #left>
-      <div class="flex flex-col gap-2 lg:gap-1 lg:text-left text-center">
-        <div class="flex flex-col lg:flex-row items-center gap-2">
-          <p class="text-sm">{{ SITE.name }} © {{ new Date().getFullYear() }}</p>
-          <UPopover mode="hover" :content="{ side: 'top' }" arrow>
-            <NuxtLink :to="`${SITE.github.repository}/commit/${buildInfo.commit}`" target="_blank">
-              <UBadge
-                color="neutral"
-                variant="subtle"
-                class="flex hover:text-highlighted hover:underline text-muted"
-              >
-                <span class="font-mono">Build {{ buildInfo.shortCommit }}</span>
-              </UBadge>
-            </NuxtLink>
-            <template #content>
-              <p>{{ timeAgo }}</p>
-            </template>
-          </UPopover>
+    <UFooter class="border-t border-default">
+      <template #left>
+        <div class="flex flex-col gap-2 lg:gap-1 lg:text-left text-center">
+          <div class="flex flex-col lg:flex-row items-center gap-2">
+            <p class="text-sm">{{ SITE.name }} © {{ new Date().getFullYear() }}</p>
+            <UPopover mode="hover" :content="{ side: 'top' }" arrow>
+              <NuxtLink :to="`${SITE.github.repository}/commit/${buildInfo.commit}`" target="_blank">
+                <UBadge
+                  color="neutral"
+                  variant="subtle"
+                  class="flex hover:text-highlighted hover:underline text-muted"
+                >
+                  <span class="font-mono">Build {{ buildInfo.shortCommit }}</span>
+                </UBadge>
+              </NuxtLink>
+              <template #content>
+                <p>{{ timeAgo }}</p>
+              </template>
+            </UPopover>
+          </div>
+          <p class="text-muted text-sm max-w-150 lg:max-w-full">
+            JimTracker no está respaldado por Riot Games y no refleja las opiniones o puntos de vista de Riot Games ni de nadie involucrado oficialmente en la producción o gestión de League of Legends.
+          </p>
         </div>
-        <p class="text-muted text-sm max-w-150 lg:max-w-full">
-          JimTracker no está respaldado por Riot Games y no refleja las opiniones o puntos de vista de Riot Games ni de nadie involucrado oficialmente en la producción o gestión de League of Legends.
-        </p>
-      </div>
-    </template>
-    <template #center />
-    <span class="text-sm">
-      <NuxtLink :to="SITE.github.repository" target="_blank" class="dark:text-blue-300 light:text-blue-500 font-semibold hover:underline">Creado</NuxtLink>
-      con ❤️ por
-      <NuxtLink :to="SITE.github.authors.ahmed.url" target="_blank" class="dark:text-emerald-300 light:text-emerald-600 font-semibold hover:underline">
-        {{ SITE.github.authors.ahmed.name }}
-      </NuxtLink>
-      y
-      <NuxtLink :to="SITE.github.authors.yizack.url" target="_blank" class="dark:text-yellow-300 light:text-yellow-600 font-semibold hover:underline">
-        {{ SITE.github.authors.yizack.name }}
-      </NuxtLink>
-    </span>
-    <template #right>
-      <NuxtLink :to="SITE.github.repository" target="_blank" class="hover:text-highlighted">
-        <Icon name="simple-icons:github" size="2rem" />
-      </NuxtLink>
-    </template>
-  </UFooter>
+      </template>
+      <template #center />
+      <span class="text-sm">
+        <NuxtLink :to="SITE.github.repository" target="_blank" class="dark:text-blue-300 light:text-blue-500 font-semibold hover:underline">Creado</NuxtLink>
+        con ❤️ por
+        <NuxtLink :to="SITE.github.authors.ahmed.url" target="_blank" class="dark:text-emerald-300 light:text-emerald-600 font-semibold hover:underline">
+          {{ SITE.github.authors.ahmed.name }}
+        </NuxtLink>
+        y
+        <NuxtLink :to="SITE.github.authors.yizack.url" target="_blank" class="dark:text-yellow-300 light:text-yellow-600 font-semibold hover:underline">
+          {{ SITE.github.authors.yizack.name }}
+        </NuxtLink>
+      </span>
+      <template #right>
+        <NuxtLink :to="SITE.github.repository" target="_blank" class="hover:text-highlighted">
+          <Icon name="simple-icons:github" size="2rem" />
+        </NuxtLink>
+      </template>
+    </UFooter>
   </div>
 </template>
