7ce4ced9db9c06d24caeeefe1662921da15fc7aa
Ahmed Rangel <ahmedrangel@outlook.com>
2026-03-08 12:55:04 -0500

    chore: deprecated error props in error.vue

--- app/error.vue
+++ app/error.vue
@@ -9,8 +9,8 @@ defineProps<{
 <template>
   <div class="w-screen h-screen flex justify-center items-center bg-neutral-400 dark:bg-neutral-950 text-lg">
     <div class="p-5 shadow bg-neutral-100 dark:bg-neutral-800 rounded">
-      <h1>Error <span class="text-primary">{{ error.statusCode }}</span></h1>
-      <h5>{{ "error_occurred" }}: {{ error.message || error.statusMessage }}</h5>
+      <h1>Error <span class="text-primary">{{ error.status }}</span></h1>
+      <h5>{{ "error_occurred" }}: {{ error.message || error.statusText || "error" }}</h5>
       <p>{{ "go_back" }}: <NuxtLink to="/" class="underline">link</NuxtLink></p>
     </div>
   </div>
