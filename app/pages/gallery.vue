4f47238f42a39b8c4620976bce45f88589cb5791
Ahmed Rangel <ahmedrangel@outlook.com>
2026-03-14 21:44:50 -0500

    fix: adjust

--- app/pages/gallery.vue
+++ app/pages/gallery.vue
@@ -21,8 +21,8 @@ const galleryURL = computed(() => window ? withQuery(`${window.location.origin}/
 
 <template>
   <main>
-    <div class="columns-2 gap-4 sm:columns-3 md:columns-5 sm:gap-8">
-      <UModal v-model:open="showModal">
+    <UModal v-model:open="showModal">
+      <div class="columns-2 gap-4 sm:columns-3 md:columns-5 sm:gap-8">
         <img
           v-for="(id, index) of images"
           :key="index"
@@ -31,18 +31,18 @@ const galleryURL = computed(() => window ? withQuery(`${window.location.origin}/
           class="rounded-lg mb-4 cursor-pointer scale-on-hover"
           @click="openImageId = id;"
         >
-        <template #content>
-          <div v-if="openImageId" class="relative">
-            <img :src="`${SITE.cdn}/gallery/${openImageId}`" class="rounded-lg max-h-[90vh] max-w-[90vw] sm:min-w-150 md:min-w-175">
-            <Icon name="lucide:x" class="absolute top-2 right-2 w-8 h-8 text-neutral-200 hover:text-neutral-300 bg-rose-700/60 hover:bg-rose-700/90 border border-rose-800/90 rounded p-1.5 shadow transition-all duratino-300 ease-in-out" @click="openImageId = null; showModal = false;" />
-          </div>
-          <UInput :value="galleryURL" class="w-full font-mono" :ui="{ trailing: 'pr-0.5' }" readonly>
-            <template #trailing>
-              <CopyButton :value="galleryURL" />
-            </template>
-          </UInput>
-        </template>
-      </UModal>
-    </div>
+      </div>
+      <template #content>
+        <div v-if="openImageId" class="relative">
+          <img :src="`${SITE.cdn}/gallery/${openImageId}`" class="rounded-lg max-h-[90vh] max-w-[90vw] sm:min-w-150 md:min-w-175">
+          <Icon name="lucide:x" class="absolute top-2 right-2 w-8 h-8 text-neutral-200 hover:text-neutral-300 bg-rose-700/60 hover:bg-rose-700/90 border border-rose-800/90 rounded p-1.5 shadow transition-all duratino-300 ease-in-out" @click="openImageId = null; showModal = false;" />
+        </div>
+        <UInput :value="galleryURL" class="w-full font-mono" :ui="{ trailing: 'pr-0.5' }" readonly>
+          <template #trailing>
+            <CopyButton :value="galleryURL" />
+          </template>
+        </UInput>
+      </template>
+    </UModal>
   </main>
 </template>
